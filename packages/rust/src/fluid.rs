c553ac4c5a0b4f53eba28a50b089a817872c0bbe
ElioStalteri <90.elio@gmail.com>
2022-07-14 11:04:08 +0000

    started project

--- packages/rust/src/fluid.rs
+++ packages/rust/src/fluid.rs
@@ -130,4 +130,38 @@ impl Fluid {
         let c = dec!(1) + dec!(6) * a;
         self.lin_solve(check_bnd, a, c);
     }
+    fn project(
+        &mut self, /* ,float *velocX, float *velocY, float *velocZ, float *p, float *div, int iter, int N */
+    ) {
+        // for (int k = 1; k < N - 1; k++) {
+        //     for (int j = 1; j < N - 1; j++) {
+        //         for (int i = 1; i < N - 1; i++) {
+        //             div[IX(i, j, k)] = -0.5f32*(
+        //                      velocX[IX(i+1, j  , k  )]
+        //                     -velocX[IX(i-1, j  , k  )]
+        //                     +velocY[IX(i  , j+1, k  )]
+        //                     -velocY[IX(i  , j-1, k  )]
+        //                     +velocZ[IX(i  , j  , k+1)]
+        //                     -velocZ[IX(i  , j  , k-1)]
+        //                 )/N;
+        //             p[IX(i, j, k)] = 0;
+        //         }
+        //     }
+        // }
+        self.lin_solve(true, dec!(1), dec!(6));
+
+        // for (int k = 1; k < N - 1; k++) {
+        //     for (int j = 1; j < N - 1; j++) {
+        //         for (int i = 1; i < N - 1; i++) {
+        //             velocX[IX(i, j, k)] -= 0.5f * (  p[IX(i+1, j, k)]
+        //                                             -p[IX(i-1, j, k)]) * N;
+        //             velocY[IX(i, j, k)] -= 0.5f * (  p[IX(i, j+1, k)]
+        //                                             -p[IX(i, j-1, k)]) * N;
+        //             velocZ[IX(i, j, k)] -= 0.5f * (  p[IX(i, j, k+1)]
+        //                                             -p[IX(i, j, k-1)]) * N;
+        //         }
+        //     }
+        // }
+        self.set_bnd();
+    }
 }
