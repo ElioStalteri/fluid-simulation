39e033996ecc3f59c4a07a95f013394663800dfd
ElioStalteri <90.elio@gmail.com>
2022-07-14 09:29:30 +0000

    added lin_solve function

--- packages/rust/src/fluid.rs
+++ packages/rust/src/fluid.rs
@@ -7,6 +7,39 @@ fn IX(x: i32, y: i32, size: i32) -> usize {
     (x + y * size) as usize
 }
 
+fn lin_solve(
+    mut v_0: Vec<vecmath::Vector2<Decimal>>,
+    v: Vec<vecmath::Vector2<Decimal>>,
+    a: Decimal,
+    c: Decimal,
+    iter: i32,
+    n: i32,
+    size: i32,
+) {
+    let cRecip = dec!(1.0) / c;
+    for _k in 0..iter {
+        for js in 1..(n - 1) {
+            for is in 1..(n - 1) {
+                let j = js as i32;
+                let i = is as i32;
+                v_0[IX(i, j, size)] = vecmath::vec2_scale(
+                    vecmath::vec2_add(
+                        v[IX(i, j, size)],
+                        vecmath::vec2_scale(
+                            vecmath::vec2_add(
+                                vecmath::vec2_add(v_0[IX(i + 1, j, size)], v_0[IX(i - 1, j, size)]),
+                                vecmath::vec2_add(v_0[IX(i, j + 1, size)], v_0[IX(i, j - 1, size)]),
+                            ),
+                            a,
+                        ),
+                    ),
+                    cRecip,
+                );
+            }
+        }
+    }
+}
+
 pub struct Fluid {
     pub size: i32,
     pub dt: Decimal,
@@ -33,5 +66,8 @@ impl Fluid {
     }
     pub fn add_density(&mut self, x: i32, y: i32, amount: Decimal) {
         self.density[IX(x, y, self.size)] += amount;
+    }
+    pub fn add_valocity(&mut self, x: i32, y: i32, v: vecmath::Vector2<Decimal>) {
+        self.v[IX(x, y, self.size)] = vecmath::vec2_add(self.v[IX(x, y, self.size)], v);
     }
 }
